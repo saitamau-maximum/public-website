100c3d849b3b231456bf4258c20a89395c3b5543
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-02-23 19:17:12 +0900

    404ページの作成

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+    title: '404 Not Found | Maximum',
+    description: 'Maximum Website',
+};
+
+export default function Custom404() {
+  return (
+    <div>
+        <h1>404 - Page Not Found</h1>
+    </div>
+  );
+}
