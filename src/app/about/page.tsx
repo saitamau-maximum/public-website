7a196490cc5b432bfe0608ae624c43e4ade1eb45
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-02-23 16:04:08 +0900

    feat: 簡易的にHomeとAboutを作成

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'サークルについて | Maximum',
+  description: 'Maximum Website',
+};
+
+export default function About() {
+  return <h1>About Page</h1>;
+}
