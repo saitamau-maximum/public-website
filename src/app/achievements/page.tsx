57d2a24f0c0254458b922018762fe5acaf3ce728
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-03-06 22:38:31 +0900

    フォルダーからmdを取得して一覧で表示する

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -6,10 +9,53 @@ export const metadata: Metadata = {
     '埼玉大学プログラミングサークル「Maximum」の大会参加報告や過去の実績をまとめたページです。',
 };
 
-export default function Achievements() {
+interface Doc {
+  slug: string;
+  content: string;
+  frontmatter: {
+    [key: string]: any;
+  };
+}
+
+const getMarkdown = async (dir: string) => {
+  const fileNames = fs.readdirSync(dir);
+  const docs = await Promise.all(
+    fileNames.map(async (fileName) => {
+      const filePath = path.join(dir, fileName);
+
+      // ファイルの中身を取得
+      const fileContents = fs.readFileSync(filePath, "utf8");
+      const { data, content } = matter(fileContents);
+
+      // ファイル名からカテゴリを取得
+      return {
+        slug: fileName.replace(".md", ""),
+        content,
+        frontmatter: data,
+      };
+    })
+  )
+  return docs;
+};
+
+export default async function Achievements() {
+  const docs: Doc[] = [];
+  // achievementの中身を取得
+  const docsDir = path.join(process.cwd(), "docs", "achievement");
+  const getDocs = await getMarkdown(docsDir);
+  docs.push(...getDocs.map((doc) => ({ ...doc })));
+
+  // 一覧を日付でソート
+  docs.sort((a, b) => (a.frontmatter.date < b.frontmatter.date ? 1 : -1));
+
   return (
     <div>
       <h1>Achievements</h1>
+      {docs.map((doc) => (
+        <div key={doc.slug}>
+          <a href={`/achievements/${doc.slug}`}>{doc.frontmatter.title}</a>
+        </div>
+      ))}
     </div>
   );
 }
