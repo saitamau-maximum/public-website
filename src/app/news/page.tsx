bf8fa837692376e433573bcb5a5b5719279bd2fb
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-03-10 16:10:51 +0900

    newsフォルダのmdファイルを一覧で表示

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,3 +1,6 @@
+import path from 'path';
+import Link from 'next/link';
+import { getMarkdowns } from '../../utils/markdown';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -6,10 +9,26 @@ export const metadata: Metadata = {
     '埼玉大学プログラミングサークル「Maximum」に関する様々なお知らせを配信しています。',
 };
 
-export default function News() {
+export default async function News() {
+  // newsの中身を取得
+  const docsDir = path.join(process.cwd(), 'docs', 'news');
+  const docs = await getMarkdowns(docsDir);
+
+  // 一覧を日付でソート
+  docs.sort((a, b) =>
+    a.frontmatter.updatedAt < b.frontmatter.updatedAt ? 1 : -1,
+  );
+
   return (
     <div>
       <h1>News</h1>
+      {docs.map((doc) => (
+        <div key={doc.slug}>
+          <Link href={`/news/${doc.slug}`}>
+            {doc.frontmatter.title}
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
