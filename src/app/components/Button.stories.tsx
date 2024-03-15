7a0e13a6cdc52ebb3e5061f6ccbc0b4002a020ce
shigek <mizokyuu114@gmail.com>
2024-03-15 12:12:01 +0900

    <button>をやめました

--- src/app/components/Button.stories.tsx
+++ src/app/components/Button.stories.tsx
@@ -1,13 +1,20 @@
-import { Meta, Story } from '@storybook/react';
 import React from 'react';
+import { Meta, Story } from '@storybook/react';
 import { Button } from './Button';
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'Components/Button',
   component: Button,
 } as Meta;
 
-const Template: Story = (args) => <Button {...args} />;
+const Template: Story = (args) => (
+  <Button {...args} onClick={action('button-click')} />
+);
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  label: 'Click Me',
+  variant: 'green',
+  href: '/',
+};
--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import clsx from 'clsx';
 import styles from './Button.module.css';
 
 interface ButtonProps {
@@ -9,10 +10,16 @@ interface ButtonProps {
 }
 
 export const Button: React.FC<ButtonProps> = ({ href, label, variant }) => {
-  const className = `${styles.button} ${variant === 'green' ? styles.greenButton : styles.grayButton}`;
+  const className = clsx(styles.button, {
+    [styles.greenButton]: variant === 'green',
+    [styles.grayButton]: variant === 'gray',
+  });
+
   return (
-    <Link href={href} passHref>
-      <button className={className}>{label}</button>
+    <Link href={href}>
+      <span className={className} role="button" tabIndex={0}>
+        {label}
+      </span>
     </Link>
   );
 };
