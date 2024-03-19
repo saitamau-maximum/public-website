91cd3ffc0859facc3900d4c0ad061e8150e69ede
shigek <mizokyuu114@gmail.com>
2024-03-20 07:39:50 +0900

    他のpropsを許容

--- src/components/LinkButton/LinkButton.tsx
+++ src/components/LinkButton/LinkButton.tsx
@@ -1,15 +1,20 @@
 import clsx from 'clsx';
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
 import React from 'react';
 import styles from './LinkButton.module.css';
 
-interface ButtonProps {
+interface ButtonProps extends Omit<LinkProps, 'href'> {
   href: string;
   variant: 'green' | 'gray';
   children: React.ReactNode;
 }
 
-export const LinkButton: React.FC<ButtonProps> = ({ href, variant , children }) => {
+export const LinkButton: React.FC<ButtonProps> = ({
+  href,
+  variant,
+  children,
+  ...props
+}) => {
   const className = clsx(
     styles.button,
     variant === 'green' && styles.greenButton,
@@ -17,7 +22,7 @@ export const LinkButton: React.FC<ButtonProps> = ({ href, variant , children }) 
   );
 
   return (
-    <Link href={href} className={className}>
+    <Link href={href} {...props} className = {className}>
       {children}
     </Link>
   );
