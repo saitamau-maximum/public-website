5e3dc7f9c7dcddecabbf92ec77bff0755c3c7bf0
nakamura <itsuki1911n@gmail.com>
2024-03-20 20:32:22 +0900

    作成

---
+++ src/components/Card/Card.stories.tsx
@@ -0,0 +1,28 @@
+import { StoryFn,Meta } from '@storybook/react';
+import React from 'react';
+import Card from './Card';
+
+
+
+export default {
+  title: 'Components/Card',
+  component: Card,
+  argTypes: {
+    title: { control: 'text' },
+    content: { control: 'text' },
+    date: { control: 'text' },
+    group: { control: 'text'},
+    link: { control: 'text' },
+  },
+} as Meta;
+
+const Template: StoryFn<typeof Card> = (args) => <Card {...args} />
+export const Default = Template.bind({});
+
+Default.args = {
+    title:'OOOOOOOOOOOOOOOOOOOOOOOOOO',
+    content:'AAAAAAAAAAAAAAAAAAAA…',
+    date:'YYYY.MM.DD',
+    group:'OOO',
+    link:'/',
+};
