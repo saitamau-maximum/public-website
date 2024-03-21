e957f63893621e737a85d3cd32fa9e1238610f0a
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-03-22 02:00:18 +0900

    max-widthを1080pxにして中央寄せ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,47 +16,51 @@ export function Home() {
             </Link>
           </div>
         </div>
-        <h1 className={style.about}>活動内容</h1>
-        <p className={style.aboutText}>
-          Maximumでは<span className={style.boldspan}>競技プログラミング</span>
-          （週２回）と<span className={style.boldspan}>Web研究会</span>
-          （週１回）の２つの活動を行っています。
-        </p>
-        <div className={style.activityCP}>
-          <div className={style.activityCPtitle}>
-            <h2>競技プログラミング</h2>
-            <p>Competitive programming</p>
+        <div className={style.contents}>
+          <h1 className={style.about}>活動内容</h1>
+          <p className={style.aboutText}>
+            Maximumでは<span className={style.boldspan}>競技プログラミング</span>
+            （週２回）と<span className={style.boldspan}>Web研究会</span>
+            （週１回）の２つの活動を行っています。
+          </p>
+          <div className={style.activityCP}>
+            <div className={style.activityCPtitle}>
+              <h2>競技プログラミング</h2>
+              <p>Competitive programming</p>
+            </div>
+            <p>
+              AtCoderやICPCに向けて毎週競技プログラミングの講義と大会の感想会などを行っています。
+            </p>
+            <LinkButton href='#' variant='gray'>
+              Learn More
+            </LinkButton>
           </div>
-          <p>
-            AtCoderやICPCに向けて毎週競技プログラミングの講義と大会の感想会などを行っています。
-          </p>
-          <LinkButton href='#' variant='gray'>
-            Learn More
-          </LinkButton>
-        </div>
-        <div className={style.activityWeb}>
-          <div className={style.activityWebtitle}>
-            <h2>Web研究会</h2>
-            <p>Web programming</p>
+          <div className={style.activityWeb}>
+            <div className={style.activityWebtitle}>
+              <h2>Web研究会</h2>
+              <p>Web programming</p>
+            </div>
+            <p>
+              Webプログラミングに関する講義とパフォーマンスチューニングの大会に向けた練習会などを行っています。
+            </p>
+            <LinkButton href='#' variant='gray'>
+              Learn More
+            </LinkButton>
           </div>
-          <p>
-            Webプログラミングに関する講義とパフォーマンスチューニングの大会に向けた練習会などを行っています。
-          </p>
-          <LinkButton href='#' variant='gray'>
-            Learn More
-          </LinkButton>
-        </div>
-        <div className={style.activityOther}>
-          <p>
-            他にも&hellip;<br/>Discordを用いた
-            <span className={style.span}>交流会</span>、
-            <span className={style.span}>勉強会</span>、
-            <span className={style.span}>サークル内模擬大会</span>
-            等々希望があれば、様々な活動を行っています。希望があれば、ほかにも様々な活動を行いたいと思っています。
-          </p>
-          <LinkButton href='#' variant='green'>
-            入会案内はこちら
-          </LinkButton>
+          <div className={style.activityOther}>
+            <p>
+              他にも&hellip;
+              <br />
+              Discordを用いた
+              <span className={style.span}>交流会</span>、
+              <span className={style.span}>勉強会</span>、
+              <span className={style.span}>サークル内模擬大会</span>
+              等々希望があれば、様々な活動を行っています。希望があれば、ほかにも様々な活動を行いたいと思っています。
+            </p>
+            <LinkButton href='#' variant='green'>
+              入会案内はこちら
+            </LinkButton>
+          </div>
         </div>
       </main>
     </div>
