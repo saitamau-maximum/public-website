431b4ec6c02177bbf3fdf8f139449967034ee384
shigek <mizokyuu114@gmail.com>
2024-03-22 22:54:17 +0900

    Footer原形

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,75 @@
+import Link from 'next/link';
+import { MdArrowOutward } from 'react-icons/md';
+import { SiX, SiGithub } from 'react-icons/si';
+import style from './Footer.module.scss';
+import { LinkButton } from '../LinkButton/LinkButton';
+
+export function Footer() {
+  return (
+    <footer className={style.footer}>
+      <div className={style.logoandbutton}>
+        <div className={style.logo}>
+          <Link href='/'>
+            <img
+              src='/logos/Maximum-logo.svg'
+              alt='Maximum'
+              width={334}
+              height={84}
+            />
+          </Link>
+          <div className={style.greenButton}>
+            <LinkButton href='/join' variant='green'>
+              入会案内はこちら
+            </LinkButton>
+          </div>
+        </div>
+      </div>
+
+      <nav className={style.navigations}>
+        <Link href='/about' className={style.navigationLink}>
+          About
+        </Link>
+        <Link href='/news' className={style.navigationLink}>
+          News
+        </Link>
+        <Link href='#' className={style.navigationLink}>
+          Join
+        </Link>
+        <Link href='/achievements' className={style.navigationLink}>
+          Achievements
+        </Link>
+        <Link href='/contact' className={style.navigationLink}>
+          Contact
+        </Link>
+        <Link href='/faq' className={style.navigationLink}>
+          Q&amp;A
+        </Link>
+        <Link
+          href='https://blog.maximum.vc'
+          target='_blank'
+          rel='noopener noreferrer'
+          className={style.navigationLink}
+        >
+          Blog
+          <MdArrowOutward />
+        </Link>
+        <Link
+          href='https://x.com/Maximum03400346'
+          target='_blank'
+          rel='noopener noreferrer'
+          className={style.navigationLink}
+        >
+          <SiX size={24} />
+        </Link>
+        <Link
+          href='https://github.com/saitamau-maximum'
+          target='_blank'
+          rel='noopener noreferrer'
+          className={style.navigationLink}
+        >
+          <SiGithub size={24} />
+        </Link>
+      </nav>
+    </footer>
+  );
+}
