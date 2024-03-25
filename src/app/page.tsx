c815b9b3e768a3d5f83a952b30cd0bd95f85f2f8
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-03-26 01:19:14 +0900

    clsxを使う

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Link from 'next/link';
 import { LinkButton } from '../components/LinkButton';
 import style from './page.module.scss';
@@ -18,12 +19,12 @@ export default function Home() {
           <h1 className={style.contentTitle}>活動内容</h1>
           <p className={style.aboutText}>
             Maximumでは
-            <span className={style.span}>
-              <span className={style.bold}>競技プログラミング</span>
+            <span className={clsx(style.span, style.bold)}>
+              競技プログラミング
             </span>
             (週2回)と
-            <span className={style.span}>
-              <span className={style.bold}>Web研究会</span>
+            <span className={clsx(style.span, style.bold)}>
+              Web研究会
             </span>
             (週1回)の2つの活動を行っています。
           </p>
@@ -36,12 +37,12 @@ export default function Home() {
               <p>Competitive programming</p>
             </div>
             <p className={style.activityCPcontent}>
-              <span className={style.span}>
-                <span className={style.bold}>AtCoder</span>
+              <span className={clsx(style.span, style.bold)}>
+                AtCoder
               </span>
               &nbsp;や&nbsp;
-              <span className={style.span}>
-                <span className={style.bold}>ICPC</span>
+              <span className={clsx(style.span, style.bold)}>
+                ICPC
               </span>
               に向けて
               <br />
