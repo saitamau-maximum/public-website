be9a3fb2a4f5a0cea0463161afc165a46b173b7c
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-04-02 14:06:36 +0900

    feat: headerのモバイルデザインを追加、ハンバーガーメニューの実装など

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -5,6 +5,9 @@ import { Header } from './Header';
 export default {
   title: 'Components/Header',
   component: Header,
+  parameters: {
+    layout: 'fullscreen',
+  },
 } as Meta<typeof Header>;
 
 export const Default: StoryFn<typeof Header> = () => <Header />;
