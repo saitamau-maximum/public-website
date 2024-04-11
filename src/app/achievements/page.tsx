c5db5c57238de39baaaf3d5a8c71aa45431051e6
nakamuraitsuki <itsuki1911n@gmail.com>
2024-04-11 13:53:41 +0900

    ステージされてませんでした

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -2,7 +2,7 @@ import path from 'path';
 import type { Metadata } from 'next';
 import { Doc, getMarkdowns } from '../../utils/markdown';
 import style from './list-styles.module.css';
-import AchievementCard from '@/components/AchievementCard';
+import AchievementList from '@/components/achievement-list/achievement-list';
 
 export const metadata: Metadata = {
   title: 'Achievements',
@@ -11,15 +11,6 @@ export const metadata: Metadata = {
 };
 
 export default async function Achievements() {
-  // achievementの中身を取得
-  const docsDir = path.join(process.cwd(), 'docs', 'achievement');
-  const docs = await getMarkdowns(docsDir);
-
-  // 一覧を日付でソート
-  docs.sort((a, b) =>
-    a.frontmatter.updatedAt < b.frontmatter.updatedAt ? 1 : -1,
-  );
-
   return (
     <div>
       <div className={style.heroBox}>
@@ -28,16 +19,7 @@ export default async function Achievements() {
       </div>
       <div className={style.container}>
         <h2 className={style.title}>参加した大会</h2>
-        {docs.map((doc) => (
-          <AchievementCard
-            key={doc.slug}
-            title={doc.frontmatter.title}
-            subtitle={doc.frontmatter.subtitle}
-            description={doc.frontmatter.description}
-            iconUrl={doc.frontmatter.iconUrl}
-            slug={doc.slug}
-          />
-        ))}
+        <AchievementList />
       </div>
     </div>
   );
