099ccbb2418a20b7970c6d49ec715f00550e96cb
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-04-11 16:30:17 +0900

    コンポーネントを作成

---
+++ src/components/BreadCrumb/BreadCrumb.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './BreadCrumb.module.scss';
+
+
+interface BreadcrumbItem {
+  items: {
+    title: string;
+    href: string;
+  }[];
+}
+
+export const BreadCrumb: React.FC<BreadcrumbItem> = ({ items }) => {
+  return (
+    <div className={styles.container}>
+      {items.map((item, index) => (
+        <React.Fragment key={index}>
+          {item.href ? (
+            <a href={item.href} className={styles.breadcrumbLink}>
+              <p className={styles.breadcrumbLink}>{item.title}</p>
+            </a>
+          ) : (
+            <p className={styles.breadcrumbLink}>{item.title}</p>
+          )}
+          {index < items.length - 1 && (
+            <p className={styles.separator}>&gt;</p>
+          )}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
