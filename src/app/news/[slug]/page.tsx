dd7068d8b99f0af0d9f32f63c436153b37597f96
nakamuraitsuki <itsuki1911n@gmail.com>
2024-04-12 13:10:13 +0900

    ニュース個別ページ作成

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -3,6 +3,9 @@ import path from 'path';
 import { parseMarkdownToHTML } from '@saitamau-maximum/markdown-processor/server';
 import matter from 'gray-matter';
 import { Metadata } from 'next';
+import style from './style.module.css';
+import { HeroImage } from '@/components/HeroImage';
+import Toc from '@/components/Toc';
 
 interface Props {
   params: {
@@ -35,11 +38,33 @@ export default async function NewsDetail({ params }: Props) {
   const fileContents = await fs.readFile(filePath, 'utf8');
   const { content, data } = matter(fileContents);
   const html = await parseMarkdownToHTML(content);
+  const updatedAt = new Date(data.updatedAt).toLocaleDateString('ja-JP', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
   return (
-    <main>
-      <h1>{data.title}</h1>
-      {/* 記事のタイトル等の動的コンテンツにXSSが発生する可能性が、信頼できるリソースからのみ提供されることとして許容する。 */}
-      <article dangerouslySetInnerHTML={{ __html: html.content }} />
-    </main>
+    <div>
+      <main>
+        <div className={style.heroBox}>
+          <HeroImage title='新着情報' type='default' blur={true} />
+        </div>
+        <div className={style.container}>
+          <div className={style.box}>
+            <div className={style.metaBox}>
+              <h2 className={style.docTitle}>{data.title}</h2>
+              <div className={style.dateGroup}>
+                <p className={style.dateText}>{updatedAt}</p>
+                <div className={style.groupBox}>{data.group}</div>
+              </div>
+            </div>
+            <hr className={style.line} />
+            {/* 記事のタイトル等の動的コンテンツにXSSが発生する可能性が、信頼できるリソースからのみ提供されることとして許容する。 */}
+            <article dangerouslySetInnerHTML={{ __html: html.content }} />
+          </div>
+          <Toc tocData={html.toc} />
+        </div>
+      </main>
+    </div>
   );
 }
