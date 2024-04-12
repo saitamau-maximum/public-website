40539ddfc76e13a60bd32e66c00baad213901b13
nakamuraitsuki <itsuki1911n@gmail.com>
2024-04-12 22:23:44 +0900

    render範囲変更

--- src/components/Toc/Toc.tsx
+++ src/components/Toc/Toc.tsx
@@ -11,25 +11,24 @@ interface TocItem {
   children: TocItem[];
 }
 
+const renderTocItems = (items: TocItem[]) => {
+  return items.map((item, index) => (
+    <div
+      key={index}
+      className={style.tocItem}
+      style={{
+        marginLeft: `${item.depth * 7.5}px`,
+        borderLeft: `${item.depth > 1 ? '1.5px solid #d3d3d3' : 'none'}`,
+      }}
+    >
+      <Link className={style.value} href={`#${item.data.id}`}>
+        {item.value}
+      </Link>
+      {item.children.length > 0 && renderTocItems(item.children)}
+    </div>
+  ));
+};
 const Toc: React.FC<{ tocData: TocItem[] }> = ({ tocData }) => {
-  const renderTocItems = (items: TocItem[]) => {
-    return items.map((item, index) => (
-      <div
-        key={index}
-        className={style.tocItem}
-        style={{
-          marginLeft: `${item.depth * 7.5}px`,
-          borderLeft: `${item.depth > 1 ? '1.5px solid #d3d3d3' : 'none'}`,
-        }}
-      >
-        <Link className={style.value} href={`#${item.data.id}`}>
-          {item.value}
-        </Link>
-        {item.children.length > 0 && renderTocItems(item.children)}
-      </div>
-    ));
-  };
-
   return (
     <div className={style.container}>
       <h3 className={style.title}>目次</h3>
@@ -38,5 +37,3 @@ const Toc: React.FC<{ tocData: TocItem[] }> = ({ tocData }) => {
     </div>
   );
 };
-
-export default Toc;
