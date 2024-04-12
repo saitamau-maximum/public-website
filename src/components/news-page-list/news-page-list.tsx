825989e145dc3f468411b50ae8115ab316e6426e
nakamuraitsuki <itsuki1911n@gmail.com>
2024-04-12 22:20:37 +0900

    fix

--- src/components/news-page-list/news-page-list.tsx
+++ src/components/news-page-list/news-page-list.tsx
@@ -6,27 +6,25 @@ import { NewsLinkCard } from '../NewsLinkCard';
 import style from './news-page-list.module.scss';
 import { getMarkdowns } from '@/utils/markdown';
 
-
 class Index extends React.Component {
   constructor(props: {} | Readonly<{}>) {
-    super(props)
+    super(props);
     this.state = {
       books: [],
       start: 0, //最初は0番目(=最新)の要素から
-      perPage: 12 //1ページには12冊表示
-    }
+      perPage: 12, //1ページには12冊表示
+    };
   }
-  const pageChange(data) {
+  pageChange(data) {
     let pageNumber = data['selected']; //選択されたページ番号
     this.setState({
       //スタート位置をページ番号 * 1ページあたりの数、とする(例えば2番を選ぶと12 * 1で12番が先頭になる、つまり13番目以降の書籍が表示される)
-      start: pageNumber * this.state.perPage 
-    })
+      start: pageNumber * this.state.perPage,
+    });
   }
-
 }
 export async function NewsPageList(this: any) {
-  const perPage: number = 2;//１ページ当たりのお知らせ数
+  const perPage: number = 2; //１ページ当たりのお知らせ数
   // newsの中身を取得
   const docsDir = path.join(process.cwd(), 'docs', 'news');
   const docs = await getMarkdowns(docsDir);
@@ -44,8 +42,6 @@ export async function NewsPageList(this: any) {
   docs.sort((a, b) =>
     a.frontmatter.updatedAt < b.frontmatter.updatedAt ? 1 : -1,
   );
-
-
 
   return (
     <div>
@@ -65,19 +61,19 @@ export async function NewsPageList(this: any) {
         ))}
       </div>
       <ReactPaginate
-  pageCount={Math.ceil(docs.length/perPage)} //総ページ数。
-  marginPagesDisplayed={1} //先頭と末尾に表示するページの数。今回は2としたので1,…今いるページの前後…後ろから1番目 のように表示されます。
-  pageRangeDisplayed={1} //上記の「今いるページの前後」の番号をいくつ表示させるかを決めます。
-  onPageChange={this.pageChange} //ページネーションのリンクをクリックしたときのイベント(詳しくは下で解説します)
-  containerClassName='pagination' //ページネーションリンクの親要素のクラス名
-  pageClassName='page-item' //各子要素(li要素)のクラス名
-  pageLinkClassName='page-link' //ページネーションのリンクのクラス名
-  activeClassName='active' //今いるページ番号のクラス名。今いるページの番号だけ太字にしたりできます 
-  disabledClassName='disabled' //先頭 or 末尾に行ったときにそれ以上戻れ(進め)なくするためのクラス
-  breakLabel='>>' // ページがたくさんあるときに表示しない番号に当たる部分をどう表示するか
-  breakClassName='page-item' // 上記の「>>」のクラス名
-  breakLinkClassName='page-link' // [>>」の中のリンクにつけるクラス
-  />
+        pageCount={Math.ceil(docs.length / perPage)} //総ページ数。
+        marginPagesDisplayed={1} //先頭と末尾に表示するページの数。今回は2としたので1,…今いるページの前後…後ろから1番目 のように表示されます。
+        pageRangeDisplayed={1} //上記の「今いるページの前後」の番号をいくつ表示させるかを決めます。
+        onPageChange={this.pageChange} //ページネーションのリンクをクリックしたときのイベント(詳しくは下で解説します)
+        containerClassName='pagination' //ページネーションリンクの親要素のクラス名
+        pageClassName='page-item' //各子要素(li要素)のクラス名
+        pageLinkClassName='page-link' //ページネーションのリンクのクラス名
+        activeClassName='active' //今いるページ番号のクラス名。今いるページの番号だけ太字にしたりできます
+        disabledClassName='disabled' //先頭 or 末尾に行ったときにそれ以上戻れ(進め)なくするためのクラス
+        breakLabel='>>' // ページがたくさんあるときに表示しない番号に当たる部分をどう表示するか
+        breakClassName='page-item' // 上記の「>>」のクラス名
+        breakLinkClassName='page-link' // [>>」の中のリンクにつけるクラス
+      />
     </div>
   );
 }
