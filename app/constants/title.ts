f244ce77e716b3b177cf8c55eb6697e69b58c2e5
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-10-05 23:49:16 +0900

    feat: title utility

---
+++ app/constants/title.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = "Maximum";
+export const SITE_TITLE_DELIMITER = " | ";
---
+++ app/utils/title.ts
@@ -0,0 +1,7 @@
+import { SITE_TITLE, SITE_TITLE_DELIMITER } from "~/constants/title";
+
+export const makePageTitle = (title: string[]) => {
+	return [...title, SITE_TITLE]
+		.filter((t) => Boolean(t))
+		.join(SITE_TITLE_DELIMITER);
+};
