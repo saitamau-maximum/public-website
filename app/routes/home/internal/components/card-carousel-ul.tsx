63862616948d30cb54039f9050553c560afbbebb
Asa <26807394+a01sa01to@users.noreply.github.com>
2026-02-22 14:42:32 +0900

    feat: scroll の grab

--- app/routes/home/internal/components/card-carousel-ul.tsx
+++ app/routes/home/internal/components/card-carousel-ul.tsx
@@ -1,4 +1,9 @@
-import type { ReactNode } from "react";
+import {
+	type MouseEventHandler,
+	type ReactNode,
+	useRef,
+	useState,
+} from "react";
 import { css } from "styled-system/css";
 import { ScrollIndicator } from "./scroll-indicator";
 
@@ -8,6 +13,71 @@ interface Props {
 }
 
 export const CardCarouselUl = ({ children, ariaLabel }: Props) => {
+	const scrollRef = useRef<HTMLUListElement>(null);
+	const [isDragging, setIsDragging] = useState(false);
+	const [isSnapEnabled, setIsSnapEnabled] = useState(true);
+	const [startX, setStartX] = useState(0);
+	const [scrollLeft, setScrollLeft] = useState(0);
+	const snapTimeoutRef = useRef<ReturnType<typeof setTimeout>>(null);
+
+	const clearSnapTimeout = () => {
+		if (snapTimeoutRef.current) {
+			clearTimeout(snapTimeoutRef.current);
+			snapTimeoutRef.current = null;
+		}
+	};
+
+	const handleMouseDown: MouseEventHandler<HTMLUListElement> = (e) => {
+		clearSnapTimeout();
+		setIsDragging(true);
+		setIsSnapEnabled(false);
+		if (scrollRef.current) {
+			// コンテナの左端からのクリック位置
+			setStartX(e.pageX - scrollRef.current.offsetLeft);
+			// クリックした時点でのスクロール位置
+			setScrollLeft(scrollRef.current.scrollLeft);
+		}
+	};
+
+	const handleMouseUp: MouseEventHandler<HTMLUListElement> = () => {
+		clearSnapTimeout();
+		setIsDragging(false);
+
+		// scroll-snap を計算し、移動する
+		// こうしないとドラッグ終了後のスクロールがスムーズにならないので
+		if (scrollRef.current) {
+			let nearestSnapPoint = 0;
+			const scrollLeft = scrollRef.current.scrollLeft;
+			const cardWidth = scrollRef.current.querySelector("li")?.clientWidth || 0;
+			nearestSnapPoint = Math.round(scrollLeft / cardWidth) * (cardWidth + 16); // 16: gap
+			scrollRef.current.scrollTo({
+				left: nearestSnapPoint,
+				behavior: "smooth",
+			});
+		}
+
+		snapTimeoutRef.current = setTimeout(() => {
+			// その後 scroll-snap を有効にする
+			setIsSnapEnabled(true);
+			snapTimeoutRef.current = null;
+		}, 500);
+	};
+
+	const handleMouseMove: MouseEventHandler<HTMLUListElement> = (e) => {
+		if (!isDragging || !scrollRef.current) return;
+		e.preventDefault();
+		clearSnapTimeout();
+		const x = e.pageX - scrollRef.current.offsetLeft;
+		const walk = (x - startX) * 1.5; // スクロールの速さを調整
+		scrollRef.current.scrollLeft = scrollLeft - walk;
+	};
+
+	// ドラッグ中の画像の選択を防止するため
+	const handleDragStart: MouseEventHandler<HTMLUListElement> = (e) => {
+		e.preventDefault();
+		clearSnapTimeout();
+	};
+
 	return (
 		<>
 			<span className={css({ srOnly: true })}>
@@ -53,11 +123,11 @@ export const CardCarouselUl = ({ children, ariaLabel }: Props) => {
 						gap: 4,
 						flexWrap: "nowrap",
 						overflowX: "auto",
+						userSelect: "none",
 						marginTop: 4,
 						paddingBottom: 2,
 						paddingLeft: "calc(50% - var(--cardWidth) / 2)",
 						paddingRight: "calc(50% - var(--cardWidth) / 2)",
-						scrollSnapType: "x mandatory",
 						// 中央でリストが止まるようにする
 						scrollPaddingLeft: "calc(50% - var(--cardWidth) / 2)",
 						"& > li": {
@@ -65,6 +135,17 @@ export const CardCarouselUl = ({ children, ariaLabel }: Props) => {
 							scrollSnapAlign: "start",
 						},
 					})}
+					ref={scrollRef}
+					onMouseDown={handleMouseDown}
+					onMouseUp={handleMouseUp}
+					onMouseLeave={handleMouseUp}
+					onMouseMove={handleMouseMove}
+					onDragStart={handleDragStart}
+					style={{
+						cursor: isDragging ? "grabbing" : "grab",
+						scrollBehavior: isDragging ? "auto" : "smooth",
+						scrollSnapType: isSnapEnabled ? "x mandatory" : "none",
+					}}
 				>
 					{children}
 				</ul>
